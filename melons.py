39b910dcdb42b1a490f60c2346a69274454e8552
Shirley Crenshaw <crenshs@gmail.com>
2022-09-30 15:52:29 -0400

    fisnished and started further study

--- melons.py
+++ melons.py
@@ -1,4 +1,7 @@
 """Classes for melon orders."""
+
+import random
+
 class AbstractMelonOrder:
     """An abstract base class that other Melon Orders inherit from."""
 
@@ -15,12 +18,23 @@ class AbstractMelonOrder:
         self.qty = qty
         self.country_code = country_code
         self.shipped = False
+    
+    def get_base_price(self, random_num):
+
+        random_num = random.randint(5, 9) # base price
+        random_base_price = random_num
         
+        # if it is not the rush hour, then we don't add the 4 dollars
+        # but if it's from 8-11 am, M-F, we add an extra $4- when call = int(), 
+        #parameter (24_hour_day, day_of_week)
+
 
     def get_total(self):
         """Calculate price, including tax."""
 
         # christmas melons is in species as an attribute
+
+        # print_this = get_base_price(self)
         base_price = 5
         
         #if christmas_melon
@@ -36,6 +50,10 @@ class AbstractMelonOrder:
         if self.order_type == "international" and self.qty < 10:
             total = total + 3
         
+        # https://www.codespeedy.com/how-to-call-method-of-another-class-in-python/
+        AbstractMelonOrder.get_base_price(self)
+        self.get_base_price()
+        # https://devnote.in/how-to-call-one-method-from-another-within-the-same-class-in-python/
         
         return total
 
@@ -106,17 +124,19 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
     #     return self.country_code
 
-class GovermentMelonOrder(AbstractMelonOrder):
+class GovernmentMelonOrder(AbstractMelonOrder):
     
-    passed_inspection = False
+    #passed_inspection = False
 
-    def __init__(self, passed, species, qty, country_code, shipped):
-        self.passed = passed
+    def __init__(self, species, qty, country_code, shipped):
+        self.passed_inspection = False
         super().__init__(species, qty, country_code, shipped)
 
     def marked_inspection(self, passed):
         if self.passed_inspection != passed:
-            passed_inspection = True
-            return passed_inspection
+            self.passed_inspection = True
+        return self.passed_inspection
 
 # is_passed = GovermentMelonOrder(passed)
+# order1 = DomesticMelonOrder("cantaloupe", 8)
+# >>> order1.get_total()
